3669145af6c32cefd97f9ae76e813bdc355259ad
Long The Xuan Nguyen <longntx@tech.est-rouge.com>
2020-05-12 11:14:06 +0700

    Fixed bug duplications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { createSkipSelf } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'change-log-convert';
+  projectPrefix = `FTD`;
   source: string;
   result: string;
-  regex = /^(FTD-([0-9]*)):(.*)((.|\s)*?- #([0-9]*))(.*)/gm;
-  regexMultilines = /\s\s\s+/gm;
-  regexMergin = /^(Merge in)(.*)(.|\s)*/gm;
-  regexForReserve = /^\*.*\)/gm;
-  regexToCheckDuplicate = /(FTD-[0-9]*)(.*)(\[PR.*\))/gm;
+  // regex = /^(FTD-([0-9]*)):(.*)((.|\s)*?- #([0-9]*))(.*)/gm;
+  // regexMultilines = /\s\s\s+/gm;
+  // regexMergin = /^(Merge in)(.*)(.|\s)*/gm;
+  // regexForReserve = /^\*.*\)/gm;
+  blahRegexToCheckDuplicate = /(FTD-[0-9]*)(.*)(\[PR.*\))/gm;
   bitBucketRepoLink = 'https://bitbucket.org/est-rouge/ftd-admin-console/pull-requests/';
 
   convert(sourceString: string): void | string {
@@ -27,16 +29,17 @@ export class AppComponent {
     let m;
     let finalRes = '';
     const res = [];
+    const regexVariables = this.generateDefaultVariables();
 
     const a = sourceString
-      .replace(this.regex, `* [$1] $3 [PR#$6](` + this.bitBucketRepoLink.trim() + `$6)`)
-      .replace(this.regexMultilines, '\n')
-      .replace(this.regexMergin, '');
+      .replace(regexVariables.regex, `* [$1]$3 [PR#$6](` + this.bitBucketRepoLink.trim() + `$6)`)
+      .replace(regexVariables.regexMultilines, '\n')
+      .replace(regexVariables.regexMergin, '');
     // tslint:disable-next-line:no-conditional-assignment
-    while ((m = this.regexForReserve.exec(a)) !== null) {
+    while ((m = regexVariables.regexForReserve.exec(a)) !== null) {
       // This is necessary to avoid infinite loops with zero-width matches
-      if (m.index === this.regexForReserve.lastIndex) {
-        this.regexForReserve.lastIndex++;
+      if (m.index === regexVariables.regexForReserve.lastIndex) {
+        regexVariables.regexForReserve.lastIndex++;
       }
 
       // The result can be accessed through the `m`-variable.
@@ -45,7 +48,7 @@ export class AppComponent {
           if (res.length === 0) {
             res.push(match);
           } else {
-            const temp = this.checkExist(match, res);
+            const temp = this.checkExist(match, res, regexVariables);
             if (temp && temp.exist) {
               res[temp.index] += ', ' + temp.result;
             } else {
@@ -61,7 +64,8 @@ export class AppComponent {
 
   private checkExist(
     source: string,
-    arrayToCheck: Array<string>
+    arrayToCheck: Array<string>,
+    regexVariables
   ): {
     exist: boolean;
     index: number;
@@ -69,9 +73,11 @@ export class AppComponent {
     result: string;
   } {
     let indexExistPR = -1;
-    const subString = this.regexToCheckDuplicate.exec(source);
+    const regExp = regexVariables.regexToCheckDuplicate;
+    regExp.lastIndex = 0;
+    const subString = regExp.exec(source);
     if (subString && subString[1]) {
-      indexExistPR = arrayToCheck.findIndex(value => value.includes(subString[1]));
+      indexExistPR = arrayToCheck.findIndex(value => value.includes(`[${subString[1]}]`));
     }
     if (indexExistPR > -1) {
       return {
@@ -86,6 +92,16 @@ export class AppComponent {
       index: -1,
       duplicate: '',
       result: '',
+    };
+  }
+
+  private generateDefaultVariables() {
+    return {
+      regex: new RegExp(`(${this.projectPrefix}-([0-9]*)):(.*)((.|\\s)*?- #([0-9]*))(.*)`, 'gm'),
+      regexMultilines: /\s\s\s+/gm,
+      regexMergin: /^(Merge in)(.*)(.|\s)*/gm,
+      regexForReserve: /^\*.*\)/gm,
+      regexToCheckDuplicate: new RegExp(`(${this.projectPrefix}-[0-9]*)(.*)(\\[PR.*\\))`, 'gm'),
     };
   }
 }
